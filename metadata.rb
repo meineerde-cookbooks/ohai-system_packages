be2368974455a072901562c6fe80f3ec7aa78620
Holger Just <hello@holgerjust.de>
2017-02-03 16:23:24 +0100

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email "hello@holgerjust.de"
 license          "MIT"
 description      "A Ohai plugin for gathering information about installed system packages"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 %w{debian ubuntu}.each do |os|
   supports os
